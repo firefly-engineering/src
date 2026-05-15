338c2628d532d94fb3b650adb9ac9b209c7c4610
Yann Hodique <yann@firefly.engineering>
2026-05-15 20:39:06 +0200

    chore: align flake inputs with nix-pins

--- flake.nix
+++ flake.nix
@@ -2,36 +2,29 @@
   description = "Firefly Engineering Monorepo";
 
   inputs = {
-    # Pin nixpkgs to same rev as turnkey for compatibility
-    nixpkgs.url = "github:NixOS/nixpkgs/e4bae1bd10c9c57b2cf517953ab70060a828ee6f";
-    flake-parts.url = "github:hercules-ci/flake-parts/80daad04eddbbf5a4d883996a73f3f542fa437ac";
-    devenv.url = "github:cachix/devenv/9bfc4a64c3a798ed8fa6cee3a519a9eac5e73cb5";
-    nix2container = {
-      url = "github:nlewo/nix2container/66f4b8a47e92aa744ec43acbb5e9185078983909";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-pins.url = "github:firefly-engineering/nix-pins";
+    nixpkgs.follows = "nix-pins/nixpkgs";
+    flake-parts.follows = "nix-pins/flake-parts";
 
-    # Turnkey toolchain management (local path until repo is public)
-    turnkey = {
-      url = "git+file:///Users/yann/src/github.com/firefly-engineering/turnkey";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.devenv.follows = "devenv";
-      inputs.nix2container.follows = "nix2container";
-    };
-
-    # Teller - versioned toolchain registry library
     teller = {
       url = "github:firefly-engineering/teller";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-pins.follows = "nix-pins";
     };
 
-    # Toolbox - package registry (provides beads, jj, go, rust, etc.)
     toolbox = {
       url = "github:firefly-engineering/toolbox";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-pins.follows = "nix-pins";
       inputs.teller.follows = "teller";
     };
+
+    turnkey = {
+      url = "github:firefly-engineering/turnkey";
+      inputs.nix-pins.follows = "nix-pins";
+      inputs.teller.follows = "teller";
+      inputs.toolbox.follows = "toolbox";
+    };
+
+    devenv.follows = "toolbox/devenv";
   };
 
   outputs =
